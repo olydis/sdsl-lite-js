440c409549d11e3db3f1f81367afd315da910620
Johannes Bader <jobader@microsoft.com>
2017-05-12 00:08:29 -0700

    upd

--- docs/index.ts
+++ docs/index.ts
@@ -36,19 +36,19 @@ $(() => {
             .append($("<th>").text("i"))
             .append($("<th>").text("Text"))
             .append($("<th>").text("ISA"))
-            .append($("<th>").text("LF"))
             .append($("<th>").text("SA"))
             .append($("<th>").text("PSI"))
+            .append($("<th>").text("LF"))
             .append($("<th>").text("BWT")));
         for (let i = 0; i < size; ++i) {
             oCsaData.append($("<tr>")
                 .append($("<td>").text(`${i}`))
                 .append($("<td>").addClass(`i${i}`).text(text.charAt(i) || nullString))
                 .append($("<td>").addClass(`i${i}`).text(isa[i]))
-                .append($("<td>").addClass(`i${sa[lf[i]]}`).text(lf[i]))
                 .append($("<td>").addClass(`i${sa[i]}`).text(sa[i]))
                 .append($("<td>").addClass(`i${sa[i]}`).text(psi[i]))
-                .append($("<td>").addClass(`i${sa[psi[i]]}`).text(bwt[i])));
+                .append($("<td>").addClass(`i${sa[lf[i]]}`).text(lf[i]))
+                .append($("<td>").addClass(`i${sa[lf[i]]}`).text(bwt[i])));
         }
         const reset = () => oCsaData.find(".hover").removeClass("hover");
         for (let i = 0; i < size; ++i) {
